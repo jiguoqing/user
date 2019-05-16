96c2cfcefa153b135805ec77c5b3ea5b2cadbe3c
Ji <jim.ji01@sap.com>
2019-05-16 21:35:28 +0800

    update

--- src/main/java/com/shiji/dao/dataobject/EmployeeDO.java
+++ src/main/java/com/shiji/dao/dataobject/EmployeeDO.java
@@ -1,12 +1,19 @@
 package com.shiji.dao.dataobject;
 
+import java.util.Date;
+
 public class EmployeeDO extends BaseDO {
 
   private static final long serialVersionUID = 1L;
+
   private String name;
-  private String job;
-  private Integer age;
+  private String englishName;
+  private String code;
   private String department;
+  private String jobTitle;
+  private String gender;
+  private String location;
+  private Integer age;
   private String email;
   private String phone;
 
@@ -18,20 +25,20 @@ public class EmployeeDO extends BaseDO {
     this.name = name;
   }
 
-  public String getJob() {
-    return job;
+  public String getEnglishName() {
+    return englishName;
   }
 
-  public void setJob(String job) {
-    this.job = job;
+  public void setEnglishName(String englishName) {
+    this.englishName = englishName;
   }
 
-  public Integer getAge() {
-    return age;
+  public String getCode() {
+    return code;
   }
 
-  public void setAge(Integer age) {
-    this.age = age;
+  public void setCode(String code) {
+    this.code = code;
   }
 
   public String getDepartment() {
@@ -40,6 +47,38 @@ public class EmployeeDO extends BaseDO {
 
   public void setDepartment(String department) {
     this.department = department;
+  }
+
+  public String getJobTitle() {
+    return jobTitle;
+  }
+
+  public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
   }
 
   public String getEmail() {
@@ -57,4 +96,14 @@ public class EmployeeDO extends BaseDO {
   public void setPhone(String phone) {
     this.phone = phone;
   }
+
+  public Date getOnboardAt() {
+    return onboardAt;
+  }
+
+  public void setOnboardAt(Date onboardAt) {
+    this.onboardAt = onboardAt;
+  }
+
+  private Date onboardAt;
 }
--- src/main/java/com/shiji/service/model/EmployeeVO.java
+++ src/main/java/com/shiji/service/model/EmployeeVO.java
@@ -1,14 +1,21 @@
 package com.shiji.service.model;
 
+import java.util.Date;
+
 public class EmployeeVO extends BaseVO {
 
   private static final long serialVersionUID = 1L;
   private String name;
-  private String job;
-  private Integer age;
+  private String englishName;
+  private String code;
   private String department;
+  private String jobTitle;
+  private String gender;
+  private String location;
+  private Integer age;
   private String email;
   private String phone;
+  private Date onboardAt;
 
   public String getName() {
     return name;
@@ -18,20 +25,20 @@ public class EmployeeVO extends BaseVO {
     this.name = name;
   }
 
-  public String getJob() {
-    return job;
+  public String getEnglishName() {
+    return englishName;
   }
 
-  public void setJob(String job) {
-    this.job = job;
+  public void setEnglishName(String englishName) {
+    this.englishName = englishName;
   }
 
-  public Integer getAge() {
-    return age;
+  public String getCode() {
+    return code;
   }
 
-  public void setAge(Integer age) {
-    this.age = age;
+  public void setCode(String code) {
+    this.code = code;
   }
 
   public String getDepartment() {
@@ -40,6 +47,38 @@ public class EmployeeVO extends BaseVO {
 
   public void setDepartment(String department) {
     this.department = department;
+  }
+
+  public String getJobTitle() {
+    return jobTitle;
+  }
+
+  public void setJobTitle(String jobTitle) {
+    this.jobTitle = jobTitle;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+  public void setLocation(String location) {
+    this.location = location;
+  }
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
   }
 
   public String getEmail() {
@@ -56,5 +95,13 @@ public class EmployeeVO extends BaseVO {
 
   public void setPhone(String phone) {
     this.phone = phone;
+  }
+
+  public Date getOnboardAt() {
+    return onboardAt;
+  }
+
+  public void setOnboardAt(Date onboardAt) {
+    this.onboardAt = onboardAt;
   }
 }
