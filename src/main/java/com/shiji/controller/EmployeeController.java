8f09695e625971a8f4018871387e316bf32a10c8
jiguoqing0112 <93326792@qq.com>
2019-06-08 22:07:48 +0800

    up

--- src/main/java/com/shiji/controller/EmployeeController.java
+++ src/main/java/com/shiji/controller/EmployeeController.java
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpServletResponse;
  * Created by jiguoqing on 2019/05/12.
  */
 @RestController
-@RequestMapping(value = "/employee", produces = "application/json; charset=UTF-8")
+@RequestMapping(value = "/api", produces = "application/json; charset=UTF-8")
 public class EmployeeController {
 
   @Autowired
