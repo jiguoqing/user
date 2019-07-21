db83797efc4fa2f378948a0a95ded74c82854128
jiguoqing0112 <93326792@qq.com>
2019-07-21 17:27:28 +0800

    up

--- src/main/java/com/shiji/controller/AssessPersonController.java
+++ src/main/java/com/shiji/controller/AssessPersonController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -25,7 +26,10 @@ public class AssessPersonController {
   }
 
   @GetMapping("/assessperson/find")
-  public AssessPersonVO find(@RequestBody AssessPersonVO assessPersonVO) {
+  public AssessPersonVO find(@RequestParam Integer employeeId, @RequestParam String phase) {
+    AssessPersonVO assessPersonVO = new AssessPersonVO();
+    assessPersonVO.setEmployeeId(employeeId);
+    assessPersonVO.setPhase(phase);
     return assessPersonService.find(assessPersonVO);
   }
 }
